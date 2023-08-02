d268eb8d7be3a6d5bba630b1d73c784c6fd005e9
Luciano Vilela Dourado <luciano.dourado@mpt.mp.br>
2023-08-02 14:30:11 -0300

    wip

--- Agenda.js
+++ Agenda.js
@@ -1,20 +1,65 @@
 import React, { cloneElement } from 'react';
-import { View, StyleSheet, FlatList, Text } from 'react-native';
+import { View, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { Icon, Text, Dialog } from '@rneui/themed';
 import Constants from 'expo-constants';
+
+
 
 import CalendarPicker from 'react-native-calendar-picker';
 
 import moment from 'moment';
 
 import { firestore, auth } from './firebase';
-import { onSnapshot, collection, addDoc, Timestamp, deleteDoc } from 'firebase/firestore';
+import { onSnapshot, collection, addDoc, Timestamp, deleteDoc, doc } from 'firebase/firestore';
+
+const EditItemDialog = ({ visible1, toggleDialog1, item }) => {
+    return (
+
+        <Dialog
+            isVisible={visible1}
+            onBackdropPress={toggleDialog1}
+        >
+            <Dialog.Title title="Dialog Title" />
+            <Text>
+                <Icon
+                    name='key'
+                    type='antdesign'
+                />
+                &nbsp;{item?.uid}
+            </Text>
+            <Text>
+                <Icon
+                    name='calendar'
+                    type='antdesign'
+                />
+                &nbsp;{moment(item?.data.seconds * 1000).format('LLL')}
+            </Text>
+            <Dialog.Actions>
+                <Dialog.Button title="incluir" onPress={() => {
+                    addDoc(collection(firestore, "agenda"), {
+
+                        data: Timestamp.fromDate(item.data.toDate()),
+                    })
+                        .then(() => toggleDialog1())
+                        .catch((err) => console.log(err));
+                }} />
+                <Dialog.Button title="cancel" onPress={toggleDialog1} />
+            </Dialog.Actions>
+        </Dialog>
+    )
+}
 
 export default function Agenda() {
 
+    const [visible1, setVisible1] = React.useState(false);
+    const toggleDialog1 = () => setVisible1(!visible1)
     const [info, setInfo] = React.useState({
         user: null,
         agenda: [],
     });
+    const [itemSel, setItemSel] = React.useState({});
+
+
     React.useEffect(() => {
 
         onSnapshot(collection(firestore, "agenda"), (snap) => {
@@ -27,7 +72,7 @@ export default function Agenda() {
                     style: {
                         backgroundColor: "#ff00FF",
                     },
-                })
+                });
             });
             setInfo({ ...info, agenda: [...result], format: [...format] });
         })
@@ -35,43 +80,50 @@ export default function Agenda() {
     return (
 
         <View style={styles.container}>
-
+            <EditItemDialog visible1={visible1} toggleDialog1={toggleDialog1} item={itemSel} />
             <CalendarPicker
                 customDatesStyles={info.format}
                 onDateChange={(data) => {
-                    addDoc(collection(firestore, "agenda"), {
+                    setItemSel({
                         user: info.user?.uid || null,
                         data: Timestamp.fromDate(data.toDate()),
-                    }).catch((err) => console.log(err));
+                    });
+                    toggleDialog1();
+
+                    /*
+                    
+                    */
                 }}
+
             />
             <FlatList
                 data={info.agenda}
                 renderItem={({ item }) => (
                     <View>
-                        <Text>
-                            key={item.uid}
-                            title={moment(item.data.seconds * 1000).format('LLL')}
-                            description={item.uid}
+                        <TouchableOpacity onPress={(e) => {
+                            deleteDoc(doc(firestore, `agenda/${item.uid}`))
+                                .catch((err) => console.log(err))
+                        }}>
+                            <View style={{ marginBottom: 10 }}>
 
-                            left={() => (
-                                <View
-                                    icon="trash-can"
-                                    size={50}
+                                <Text>
+                                    <Icon
+                                        name='key'
+                                        type='antdesign'
+                                    />
+                                    &nbsp;{item.uid}
+                                </Text>
+                                <Text>
+                                    <Icon
+                                        name='calendar'
+                                        type='antdesign'
+                                    />
+                                    &nbsp;{moment(item.data.seconds * 1000).format('LLL')}
+                                </Text>
 
-                                    onPress={(e) => {
-                                        
-                                        firestore()
-                                            .collection('agenda')
-                                            .doc(item.uid)
-                                            .delete()
-                                            .catch((err) => console.log(err));
-                                    }}
-                                />
-                            )
+                            </View>
 
-                            }
-                        </Text>
+                        </TouchableOpacity>
                     </View>)}
             />
 
